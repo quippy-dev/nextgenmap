239dd5124a0b78acd52263d21d6f7b91c3c78169
quippy-dev <131914530+quippy-dev@users.noreply.github.com>
2023-04-28 09:13:47 -0600

    lets make it Qt6

--- nextgenmap.py
+++ nextgenmap.py
@@ -3,9 +3,10 @@ import os
 import tempfile
 import subprocess
 import psutil
-from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QTabWidget, QWidget, QLabel, QLineEdit, QComboBox, QPushButton, QTextEdit, QTableWidget, QTableWidgetItem
-from PyQt5.QtCore import QThread, pyqtSignal
-from PyQt5.QtGui import QTextCursor
+from nextgenmap_ui import Ui_MainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStyleFactory, QTableWidgetItem
+from PyQt6.QtCore import QThread, pyqtSignal
+from PyQt6.QtGui import QTextCursor
 from nmap import PortScanner, PortScannerError
 
 class NmapScanThread(QThread):
@@ -14,101 +15,58 @@ class NmapScanThread(QThread):
     scan_error = pyqtSignal(str)
 
     def __init__(self, target, arguments, parent=None):
-        super(NmapScanThread, self).__init__(parent)
+        super().__init__(parent)
         self.target = target
         self.arguments = arguments
-        self.process = None  # Store the process
+        self.process = None
 
     def run(self):
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
             temp_filename = temp_file.name
-            temp_file.close()  # Close the file before running nmap
 
-            command = f"nmap {self.arguments} -oX {temp_filename} {self.target}"
-            print (command)
-            self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
-            
-            while True:
-                line = self.process.stdout.readline()
-                if not line:
-                    break
-                self.output_received.emit(line.strip())
-            
-            self.process.wait()
-            
-            nm = PortScanner()
-            with open(temp_filename, 'r') as xml_file:
-                try:
-                    nm.analyse_nmap_xml_scan(xml_file.read())
-                    self.scan_finished.emit(nm)
-                except PortScannerError as e:
-                    self.scan_error.emit(str(e))
-            os.remove(temp_filename)
-            self.scan_finished.emit(nm)  # Emit the nm object
-            self.process = None
+        command = f"nmap {self.arguments} -oX {temp_filename} {self.target}"
+        self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+        
+        while (line := self.process.stdout.readline()):
+            self.output_received.emit(line.strip())
+        
+        self.process.wait()
+        nm = PortScanner()
+        with open(temp_filename, 'r') as xml_file:
+            try:
+                nm.analyse_nmap_xml_scan(xml_file.read())
+                self.scan_finished.emit(nm)
+            except PortScannerError as e:
+                self.scan_error.emit(str(e))
+        os.remove(temp_filename)
+        self.process = None
 
     def terminate(self):
-        if self.process is not None:
+        if self.process:
             process = psutil.Process(self.process.pid)
             for child_proc in process.children(recursive=True):
                 child_proc.kill()
             process.kill()
 
-class nextgeNmapGUI(QMainWindow):
+class NextGeNmapGUI(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(nextgeNmapGUI, self).__init__()
-        self.setWindowTitle("nextgeNmap")
+        super().__init__()
         self.nm = None
         self.scan_thread = None
+        self.setupUi(self)
         self.init_ui()
 
     def init_ui(self):
-        main_widget = QWidget(self)
-        main_layout = QVBoxLayout(main_widget)
-
-        self.target_entry = QLineEdit()
-        self.profile_combobox = QComboBox()
-        self.profile_combobox.addItems(["Intense scan", "Intense scan, all TCP ports", "Intense scan, no ping", "Ping scan", "Quick scan", "Quick scan plus", "Quick traceroute", "Regular scan", "Slow comprehensive scan"])
-        self.scan_button = QPushButton("Scan")
+        self.profile_combobox.addItems(["Intense scan", "Intense scan, all TCP ports", "Intense scan, no ping", "Ping scan", "Quick scan", 
+                                        "Quick scan plus", "Quick traceroute", "Regular scan", "Slow comprehensive scan"])
         self.scan_button.clicked.connect(self.start_scan)
-        self.cancel_button = QPushButton("Cancel")
         self.cancel_button.setEnabled(False)
         self.cancel_button.clicked.connect(self.cancel_scan)
-        self.nmap_output = QTextEdit()
-        self.ports_hosts = QTableWidget()
-        self.command_entry = QLineEdit()
-
-        self.update_command()
         self.target_entry.textChanged.connect(self.update_command)
         self.target_entry.textChanged.connect(self.update_target)
         self.command_entry.textChanged.connect(self.update_target_from_command)
         self.profile_combobox.currentIndexChanged.connect(self.update_command)
-        self.ports_hosts.setColumnCount(6)
-        self.ports_hosts.setHorizontalHeaderLabels(["Status", "Port", "Protocol", "State", "Service", "Version"])
-        
-        target_layout = QHBoxLayout()
-        target_layout.addWidget(QLabel("Target:"))
-        target_layout.addWidget(self.target_entry)
-        target_layout.addWidget(QLabel("Profile:"))
-        target_layout.addWidget(self.profile_combobox)
-        target_layout.addWidget(self.scan_button)
-        target_layout.addWidget(self.cancel_button)
-        main_layout.addLayout(target_layout)
-
-        command_layout = QHBoxLayout()
-        command_layout.addWidget(QLabel("Command:"))
-        command_layout.addWidget(self.command_entry)
-        main_layout.addLayout(command_layout)
-
-        tabs = QTabWidget()
-        tabs.addTab(self.nmap_output, "Nmap Output")
-        tabs.addTab(self.ports_hosts, "Ports/Hosts")
-        
-        main_layout.addWidget(tabs)
-
-        self.setCentralWidget(main_widget)
-        self.setWindowTitle("nextgeNmap")
-        self.setFixedSize(800, 600)
+        self.update_command()
 
     def start_scan(self):
         if self.scan_thread and self.scan_thread.isRunning():
@@ -122,8 +80,8 @@ class nextgeNmapGUI(QMainWindow):
         if len(command_parts) < 3 or command_parts[-1].startswith("-"):
             return
 
-        target = command_parts.pop() # pop to remove target
-        command_parts.pop(0) # pop to remove 'nmap'
+        target = command_parts.pop()
+        _ = command_parts.pop(0)
         arguments = " ".join(command_parts)
 
         self.scan_thread = NmapScanThread(target, arguments)
@@ -144,12 +102,12 @@ class nextgeNmapGUI(QMainWindow):
     def update_target_from_command(self):
         command = self.command_entry.text()
         command_parts = command.split()
-        if command_parts:
-            # Find the last non-option argument
-            for i in range(len(command_parts)-1, -1, -1):
-                if not command_parts[i].startswith('-'):
-                    self.target_entry.setText(command_parts[i])
-                    return
+        if len(command_parts) > 1:
+            if command_parts[0] != "nmap":
+                for i in range(len(command_parts)-1, -1, -1):
+                    if not command_parts[i].startswith('-'):
+                        self.target_entry.setText(command_parts[i])
+                        return
                 
     def update_command(self):
         profiles = {
@@ -169,7 +127,8 @@ class nextgeNmapGUI(QMainWindow):
 
         if profile in profiles:
             arguments = profiles[profile]
-            self.command_entry.setText(f"nmap {arguments} {target}")
+            if target is not None:
+                self.command_entry.setText(f"nmap {arguments} {target}")
         
     def cancel_scan(self):
         if self.scan_thread and self.scan_thread.isRunning():
@@ -178,41 +137,44 @@ class nextgeNmapGUI(QMainWindow):
             self.scan_thread = None
 
     def update_output(self, text):
-        self.nmap_output.append(text)
-        self.nmap_output.moveCursor(QTextCursor.End)
-        #self.nmap_output.append(text)
+        self.nmap_output_text.appendPlainText(text)
+        self.nmap_output_text.moveCursor(QTextCursor.MoveOperation.End)
 
     def scan_finished(self, nm):
-        self.nm = nm  # Store the nm object
+        self.nm = nm
         self.populate_ports_hosts_grid()
         self.cancel_button.setEnabled(False)
 
     def scan_error(self, error_message):
-        self.nmap_output.append("Scan aborted.")
-        #self.nmap_output.append(f"Error: {error_message}")
+        self.nmap_output_text.appendPlainText("Scan aborted.")
 
     def populate_ports_hosts_grid(self):
-        self.ports_hosts.setRowCount(0)
+        self.ports_hosts_table.setRowCount(0)
         row_num = 0
         for host in self.nm.all_hosts():
-            for protocol in self.nm[host]['tcp'].keys():
-                result = self.nm[host]['tcp'][protocol]
-                self.ports_hosts.insertRow(row_num)
-                self.ports_hosts.setItem(row_num, 0, QTableWidgetItem("-"))
-                self.ports_hosts.setItem(row_num, 1, QTableWidgetItem(str(protocol)))
-                self.ports_hosts.setItem(row_num, 2, QTableWidgetItem("tcp"))
-                self.ports_hosts.setItem(row_num, 3, QTableWidgetItem(result['state']))
-                self.ports_hosts.setItem(row_num, 4, QTableWidgetItem(result['name']))
-                product_version = f"{result['product']} {result['version']}" if 'version' in result else result['product']
-                self.ports_hosts.setItem(row_num, 5, QTableWidgetItem(product_version))
-                #self.ports_hosts.setItem(row_num, 5, QTableWidgetItem(result['product']))
-                row_num += 1
+            print (host)
+            if self.nm[host].state() == "up":
+                for proto in self.nm[host].all_protocols():
+                    print (proto)
+                    lport = self.nm[host][proto].keys()
+                    for port in lport:
+                        status = self.nm[host][proto][port]["state"]
+                        service = self.nm[host][proto][port]["name"]
+                        version = self.nm[host][proto][port]["product"] + " " + self.nm[host][proto][port]["version"]
+                        self.ports_hosts_table.insertRow(row_num)
+                        self.ports_hosts_table.setItem(row_num, 0, QTableWidgetItem("Host " + host))
+                        self.ports_hosts_table.setItem(row_num, 1, QTableWidgetItem(str(port)))
+                        self.ports_hosts_table.setItem(row_num, 2, QTableWidgetItem(proto))
+                        self.ports_hosts_table.setItem(row_num, 3, QTableWidgetItem(status))
+                        self.ports_hosts_table.setItem(row_num, 4, QTableWidgetItem(service))
+                        self.ports_hosts_table.setItem(row_num, 5, QTableWidgetItem(version))
+                        row_num += 1
+                        self.ports_hosts_table.resizeColumnsToContents()
 
-def main():
-    app = QApplication(sys.argv)
-    gui = nextgeNmapGUI()
-    gui.show()
-    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    app.setStyle(QStyleFactory.create('Fusion'))
+    window = NextGeNmapGUI()
+    window.show()
+    sys.exit(app.exec())
