444e1f40d928a50ca29a751ecad805de9c081ad1
quippy-dev <131914530+quippy-dev@users.noreply.github.com>
2023-04-27 20:40:42 -0600

    large refactor, really needed Qt5

--- nextgenmap.py
+++ nextgenmap.py
@@ -1,202 +1,218 @@
-import tkinter as tk
-from tkinter import ttk
-import nmap
-import threading
+import sys
+import os
+import tempfile
+import subprocess
+import psutil
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QVBoxLayout, QTabWidget, QWidget, QLabel, QLineEdit, QComboBox, QPushButton, QTextEdit, QTableWidget, QTableWidgetItem
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QTextCursor
+from nmap import PortScanner, PortScannerError
 
-class NmapGUI:
-    def __init__(self):
-        self.root = tk.Tk()
-        self.root.title("nextgeNmap")
-        self.create_widgets()
-        self.root.mainloop()
+class NmapScanThread(QThread):
+    output_received = pyqtSignal(str)
+    scan_finished = pyqtSignal(PortScanner)
+    scan_error = pyqtSignal(str)
 
-    def create_widgets(self):
-        self.create_main_frame()
-        self.create_top_row()
-        self.create_command_entry()
-        self.create_tabs()
+    def __init__(self, target, arguments, parent=None):
+        super(NmapScanThread, self).__init__(parent)
+        self.target = target
+        self.arguments = arguments
+        self.process = None  # Store the process
 
-    def create_main_frame(self):
-        self.main_frame = ttk.Frame(self.root, padding="10 10 10 10")
-        self.main_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        self.main_frame.columnconfigure(0, weight=1)
-        self.main_frame.rowconfigure(0, weight=1)
+    def run(self):
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_filename = temp_file.name
+            temp_file.close()  # Close the file before running nmap
 
-    def create_top_row(self):
-        target_label = ttk.Label(self.main_frame, text="Target:")
-        target_label.grid(column=0, row=0, sticky=tk.W)
-
-        self.target_entry = ttk.Entry(self.main_frame)
-        self.target_entry.grid(column=1, row=0, sticky=(tk.W, tk.E))
-        self.target_entry.bind("<KeyRelease>", self.update_command_entry)
-
-        profile_label = ttk.Label(self.main_frame, text="Profile:")
-        profile_label.grid(column=2, row=0)
-
-        self.profile_combobox = ttk.Combobox(self.main_frame, values=[
-            "Intense scan",
-            "Intense scan, all TCP ports",
-            "Intense scan, no ping",
-            "Ping scan",
-            "Quick scan",
-            "Quick scan plus",
-            "Quick traceroute",
-            "Regular scan",
-            "Slow comprehensive scan"
-        ], state="readonly")
-        self.profile_combobox.current(0)
-        self.profile_combobox.bind("<<ComboboxSelected>>", self.update_command_entry)
-        self.profile_combobox.grid(column=3, row=0, sticky=(tk.W, tk.E))
-
-        self.scan_button = ttk.Button(self.main_frame, text="Scan", command=self.run_scan)
-        self.scan_button.grid(column=4, row=0, sticky=tk.E)
-
-        self.cancel_button = ttk.Button(self.main_frame, text="Cancel", command=self.cancel_scan)
-        self.cancel_button.grid(column=5, row=0, sticky=tk.E)
-
-        self.main_frame.grid_columnconfigure(1, weight=1)
-        self.main_frame.grid_columnconfigure(3, weight=1)
-
-    def update_command_entry(self, event=None):
-        target = self.target_entry.get()
-        profile = self.profile_combobox.get()
-        command = f"nmap {target}"
-
-        if profile == "Intense scan, all TCP ports":
-            command = f"nmap -p 1-65535 -T4 -A -v {target}"
-        elif profile == "Intense scan, no ping":
-            command = f"nmap -T4 -A -v -Pn {target}"
-        elif profile == "Ping scan":
-            command = f"nmap -sn {target}"
-        elif profile == "Quick scan":
-            command = f"nmap -T4 -F {target}"
-        elif profile == "Quick scan plus":
-            command = f"nmap -sV -T4 -O -F --version-light {target}"
-        elif profile == "Quick traceroute":
-            command = f"nmap -sn --traceroute {target}"
-        elif profile == "Regular scan":
-            command = f"nmap {target}"
-        elif profile == "Slow comprehensive scan":
-            command = f"nmap -sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 {target}"
-
-        self.command_entry.delete(0, tk.END)
-        self.command_entry.insert(0, command)
-
-    def create_command_entry(self):
-        self.command_entry = ttk.Entry(self.main_frame)
-        self.command_entry.grid(column=0, row=1, columnspan=6, sticky=(tk.W, tk.E))
-
-    def create_tabs(self):
-        self.tabControl = ttk.Notebook(self.main_frame)
-        
-        self.nmap_output_tab = ttk.Frame(self.tabControl)
-        self.tabControl.add(self.nmap_output_tab, text="Nmap Output")
-
-        self.ports_hosts_tab = ttk.Frame(self.tabControl)
-        self.tabControl.add(self.ports_hosts_tab, text="Ports/Hosts")
-        
-        self.tabControl.grid(column=0, row=2, columnspan=6, sticky=(tk.W, tk.E, tk.N, tk.S))
-        self.create_nmap_output_tab()
-        self.create_ports_hosts_tab()
-
-    def create_nmap_output_tab(self):
-        self.nmap_output_text = tk.Text(self.nmap_output_tab, wrap=tk.WORD, state="disabled")
-        self.nmap_output_text.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        scrollbar = ttk.Scrollbar(self.nmap_output_tab, orient=tk.VERTICAL, command=self.nmap_output_text.yview)
-        scrollbar.grid(column=1, row=0, sticky=(tk.N, tk.S))
-        self.nmap_output_text.configure(yscrollcommand=scrollbar.set)
-
-    def create_ports_hosts_tab(self):
-        self.ports_hosts_frame = ttk.Frame(self.ports_hosts_tab)
-        self.ports_hosts_frame.grid(column=0, row=0, sticky=(tk.W, tk.E, tk.N, tk.S))
-        
-        # Add the column headers
-        headers = ["Status", "Port", "Protocol", "State", "Service", "Version"]
-        for col_num, header in enumerate(headers):
-            label = ttk.Label(self.ports_hosts_frame, text=header, relief=tk.RAISED, padding="5 5 5 5")
-            label.grid(row=0, column=col_num, sticky="nsew")
-
-    def run_scan(self):
-        self.nmap_output_text.configure(state="normal")
-        self.nmap_output_text.delete("1.0", tk.END)
-        self.nmap_output_text.configure(state="disabled")
-
-        target = self.target_entry.get()
-        command = self.command_entry.get()
-        parsed_command = command.split()
-
-        self.nm = nmap.PortScanner()
-        self.scan_thread = threading.Thread(target=self.scan_and_display, args=(target, parsed_command))
-        self.scan_thread.daemon = True
-        self.scan_thread.start()
-
-
-    def cancel_scan(self):
-        if self.scan_thread and self.scan_thread.is_alive():
-            self.nm.process.stop()
-            self.nmap_output_text.insert(tk.END, "\n\nScan canceled.")
-
-    def scan_and_display(self, target, parsed_command):
-        try:
-            self.nm.scan(hosts=target, arguments=' '.join(parsed_command[1:]))
-            self.display_nmap_output()
-            self.root.after(1000, self.populate_ports_hosts_grid)
-        except Exception as e:
-            self.nmap_output_text.insert(tk.END, f"\n\nError: {str(e)}")
-
-    def callback_result(self, host, scan_result):
-        self.root.after(0, self.display_nmap_output, host, scan_result)
-
-    def display_nmap_output(self):
-        for host in self.nm.all_hosts():
-            self.update_nmap_output(f"Host: {host}\n")
+            command = f"nmap {self.arguments} -oX {temp_filename} {self.target}"
+            print (command)
+            self.process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
             
-            for protocol in self.nm[host]['tcp'].keys():
-                result = self.nm[host]['tcp'][protocol]
-                self.update_nmap_output(f"Port: {protocol}\tState: {result['state']}\tService: {result['name']}\tVersion: {result['product']}\n")
+            while True:
+                line = self.process.stdout.readline()
+                if not line:
+                    break
+                self.output_received.emit(line.strip())
+            
+            self.process.wait()
+            
+            nm = PortScanner()
+            with open(temp_filename, 'r') as xml_file:
+                try:
+                    nm.analyse_nmap_xml_scan(xml_file.read())
+                    self.scan_finished.emit(nm)
+                except PortScannerError as e:
+                    self.scan_error.emit(str(e))
+            os.remove(temp_filename)
+            self.scan_finished.emit(nm)  # Emit the nm object
+            self.process = None
+
+    def terminate(self):
+        if self.process is not None:
+            process = psutil.Process(self.process.pid)
+            for child_proc in process.children(recursive=True):
+                child_proc.kill()
+            process.kill()
+
+class nextgeNmapGUI(QMainWindow):
+    def __init__(self):
+        super(nextgeNmapGUI, self).__init__()
+        self.setWindowTitle("nextgeNmap")
+        self.nm = None
+        self.scan_thread = None
+        self.init_ui()
+
+    def init_ui(self):
+        main_widget = QWidget(self)
+        main_layout = QVBoxLayout(main_widget)
+
+        self.target_entry = QLineEdit()
+        self.profile_combobox = QComboBox()
+        self.profile_combobox.addItems(["Intense scan", "Intense scan, all TCP ports", "Intense scan, no ping", "Ping scan", "Quick scan", "Quick scan plus", "Quick traceroute", "Regular scan", "Slow comprehensive scan"])
+        self.scan_button = QPushButton("Scan")
+        self.scan_button.clicked.connect(self.start_scan)
+        self.cancel_button = QPushButton("Cancel")
+        self.cancel_button.setEnabled(False)
+        self.cancel_button.clicked.connect(self.cancel_scan)
+        self.nmap_output = QTextEdit()
+        self.ports_hosts = QTableWidget()
+        self.command_entry = QLineEdit()
+
+        self.update_command()
+        self.target_entry.textChanged.connect(self.update_command)
+        self.target_entry.textChanged.connect(self.update_target)
+        self.command_entry.textChanged.connect(self.update_target_from_command)
+        self.profile_combobox.currentIndexChanged.connect(self.update_command)
+        self.ports_hosts.setColumnCount(6)
+        self.ports_hosts.setHorizontalHeaderLabels(["Status", "Port", "Protocol", "State", "Service", "Version"])
+        
+        target_layout = QHBoxLayout()
+        target_layout.addWidget(QLabel("Target:"))
+        target_layout.addWidget(self.target_entry)
+        target_layout.addWidget(QLabel("Profile:"))
+        target_layout.addWidget(self.profile_combobox)
+        target_layout.addWidget(self.scan_button)
+        target_layout.addWidget(self.cancel_button)
+        main_layout.addLayout(target_layout)
+
+        command_layout = QHBoxLayout()
+        command_layout.addWidget(QLabel("Command:"))
+        command_layout.addWidget(self.command_entry)
+        main_layout.addLayout(command_layout)
+
+        tabs = QTabWidget()
+        tabs.addTab(self.nmap_output, "Nmap Output")
+        tabs.addTab(self.ports_hosts, "Ports/Hosts")
+        
+        main_layout.addWidget(tabs)
+
+        self.setCentralWidget(main_widget)
+        self.setWindowTitle("nextgeNmap")
+        self.setFixedSize(800, 600)
+
+    def start_scan(self):
+        if self.scan_thread and self.scan_thread.isRunning():
+            return
+
+        command = self.command_entry.text().strip()
+        if not command:
+            return
+
+        command_parts = command.split()
+        if len(command_parts) < 3 or command_parts[-1].startswith("-"):
+            return
+
+        target = command_parts.pop() # pop to remove target
+        command_parts.pop(0) # pop to remove 'nmap'
+        arguments = " ".join(command_parts)
+
+        self.scan_thread = NmapScanThread(target, arguments)
+        self.scan_thread.output_received.connect(self.update_output)
+        self.scan_thread.scan_finished.connect(self.scan_finished)
+        self.scan_thread.scan_error.connect(self.scan_error)
+        self.scan_thread.start()
+        self.cancel_button.setEnabled(True)
+
+    def update_target(self):
+        target = self.target_entry.text()
+        command = self.command_entry.text()
+        command_parts = command.split()
+        if command_parts and command_parts[-1] == "{target}":
+            command_parts[-1] = target
+            self.command_entry.setText(" ".join(command_parts))
+
+    def update_target_from_command(self):
+        command = self.command_entry.text()
+        command_parts = command.split()
+        if command_parts:
+            # Find the last non-option argument
+            for i in range(len(command_parts)-1, -1, -1):
+                if not command_parts[i].startswith('-'):
+                    self.target_entry.setText(command_parts[i])
+                    return
                 
-            self.update_nmap_output("\n")
-                    
-    def update_nmap_output(self, text):
-        self.nmap_output_text.configure(state="normal")
-        self.nmap_output_text.insert(tk.END, text)
-        self.nmap_output_text.see(tk.END)
-        self.nmap_output_text.configure(state="disabled")
+    def update_command(self):
+        profiles = {
+            "Intense scan": "-T4 -A -v",
+            "Intense scan, all TCP ports": "-p 1-65535 -T4 -A -v",
+            "Intense scan, no ping": "-T4 -A -v -Pn",
+            "Ping scan": "-sn",
+            "Quick scan": "-T4 -F",
+            "Quick scan plus": "-sV -T4 -O -F --version-light",
+            "Quick traceroute": "-sn --traceroute",
+            "Regular scan": "",
+            "Slow comprehensive scan": "-sS -sU -T4 -A -v -PE -PP -PS80,443 -PA3389 -PU40125 -PY -g 53 --script \"default or (discovery and safe)\""
+        }
+
+        profile = self.profile_combobox.currentText()
+        target = self.target_entry.text()
+
+        if profile in profiles:
+            arguments = profiles[profile]
+            self.command_entry.setText(f"nmap {arguments} {target}")
+        
+    def cancel_scan(self):
+        if self.scan_thread and self.scan_thread.isRunning():
+            self.scan_thread.terminate()
+            self.scan_thread.wait()
+            self.scan_thread = None
+
+    def update_output(self, text):
+        self.nmap_output.append(text)
+        self.nmap_output.moveCursor(QTextCursor.End)
+        #self.nmap_output.append(text)
+
+    def scan_finished(self, nm):
+        self.nm = nm  # Store the nm object
+        self.populate_ports_hosts_grid()
+        self.cancel_button.setEnabled(False)
+
+    def scan_error(self, error_message):
+        self.nmap_output.append("Scan aborted.")
+        #self.nmap_output.append(f"Error: {error_message}")
 
     def populate_ports_hosts_grid(self):
-        # Clear the existing rows
-        for widget in self.ports_hosts_frame.grid_slaves():
-            if int(widget.grid_info()["row"]) > 0:
-                widget.destroy()
-
-        row_num = 1
+        self.ports_hosts.setRowCount(0)
+        row_num = 0
         for host in self.nm.all_hosts():
             for protocol in self.nm[host]['tcp'].keys():
                 result = self.nm[host]['tcp'][protocol]
-                
-                # if result['state'] == "open":
-                #     status_icon = "green_circle_icon.png"  # Replace with an appropriate icon file
-                #     status_label = ttk.Label(self.ports_hosts_frame, image=status_icon)
-                # else:
-                status_label = ttk.Label(self.ports_hosts_frame, text="-")
-                status_label.grid(row=row_num, column=0)
-                
-                port_label = ttk.Label(self.ports_hosts_frame, text=protocol)
-                port_label.grid(row=row_num, column=1)
-                
-                proto_label = ttk.Label(self.ports_hosts_frame, text="tcp")
-                proto_label.grid(row=row_num, column=2)
-                
-                state_label = ttk.Label(self.ports_hosts_frame, text=result['state'])
-                state_label.grid(row=row_num, column=3)
-                
-                service_label = ttk.Label(self.ports_hosts_frame, text=result['name'])
-                service_label.grid(row=row_num, column=4)
-                
-                version_label = ttk.Label(self.ports_hosts_frame, text=result['product'])
-                version_label.grid(row=row_num, column=5)
-                
+                self.ports_hosts.insertRow(row_num)
+                self.ports_hosts.setItem(row_num, 0, QTableWidgetItem("-"))
+                self.ports_hosts.setItem(row_num, 1, QTableWidgetItem(str(protocol)))
+                self.ports_hosts.setItem(row_num, 2, QTableWidgetItem("tcp"))
+                self.ports_hosts.setItem(row_num, 3, QTableWidgetItem(result['state']))
+                self.ports_hosts.setItem(row_num, 4, QTableWidgetItem(result['name']))
+                product_version = f"{result['product']} {result['version']}" if 'version' in result else result['product']
+                self.ports_hosts.setItem(row_num, 5, QTableWidgetItem(product_version))
+                #self.ports_hosts.setItem(row_num, 5, QTableWidgetItem(result['product']))
                 row_num += 1
 
+def main():
+    app = QApplication(sys.argv)
+    gui = nextgeNmapGUI()
+    gui.show()
+    sys.exit(app.exec_())
+
 if __name__ == "__main__":
-    app = NmapGUI()
+    main()
